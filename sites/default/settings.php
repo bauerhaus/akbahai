957552e8dcb7ffda8fe2a8e435f4af6a21f3e06a
adam bauer <adam.bauer@bauerhaus.ws>
2020-06-22 03:22:56 +0000

    settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -5,7 +5,6 @@ $settings['hash_salt'] = 'Mm9EjqmiIH8zmw8zqNIqjc_szsmMqMtxyoJNF9KJW8fcTEkQn9XshF
 
 $settings['update_free_access'] = FALSE;
 
-
 /**
  * Load services definition file.
  */
@@ -23,7 +22,6 @@ $settings['entity_update_batch_size'] = 50;
 $settings['entity_update_backup'] = TRUE;
 
 $settings['migrate_node_migrate_type_classic'] = FALSE;
-
 
 
 $settings['config_sync_directory'] = 'sites/default/files/config_G_Nk8z66uVBhyvUBzOodO0xP7fNYt667LAnsI74EAz7aPT9yrtvxsHDtikZ2lJNlfI4dN4N8Sg/sync';
